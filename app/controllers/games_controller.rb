9d9a03d4e5e29a4bfb4b5fadb9f8505e53965558
Wilson <wphoong@gmail.com>
2017-04-24 22:26:58 +0000

    added destroy method and added edit and destroy buttons to show page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -29,6 +29,12 @@ class GamesController < ApplicationController
       redirect_to root_path
     end
 
+    def destroy
+      @game = Game.find(params[:id])
+      @game.destroy
+      redirect_to root_path
+    end
+
     private
 
     def game_params
